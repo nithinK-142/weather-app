f2975ba2a2068440d30d392ac0300601d542c162
nithin <nithin.sagar.359@gmail.com>
2023-06-01 16:25:46 +0530

    initial commit.

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
-// Get the current time and display it
-var currentTime = moment().tz("{your_time_zone}").format("h:mm:ss A");
-$("#time").text(currentTime);
+// Get the current date
+var currentDate = moment().format("dddd, MMMM Do YYYY");
 
-// Get the current date and display it
-var currentDate = moment().tz("{your_time_zone}").format("dddd, MMMM Do YYYY");
+// Function to update the time
+function updateTime() {
+  var currentTime = moment().format("h:mm:ss A");
+  $("#time").text(currentTime);
+}
+
+updateTime();
+
+// Display the current date
 $("#date").text(currentDate);
+
+// Update the time every second
+setInterval(updateTime, 1000);
+
 
 // Get the weather for the specified city and display it
 function getWeather() {
-  var apiKey = "API_KEY";
+
   var city = $('#city').val();
+
+  var apiKey = "API_KEY";
+
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
-  $.getJSON(apiUrl, function(data) {
+
+    $.getJSON(apiUrl)
+  .done(function(data) {
+
+    // Process weather data and update the display
     var temp = data.main.temp;
     var humidity = data.main.humidity;
     var wind = data.wind.speed;
     var weather = data.weather[0].description;
-    var weatherText = "Current temperature in " + city + ": " + temp + "°C.";
-    var humidityText = "Humidity: " + humidity + "%.";
-    var windText = "Wind speed: " + wind + " m/s.";
+
+    var cityName = "City : " + city.toUpperCase();
+    var weatherText = "Current Temperature: " + temp + "°C.";
+    var humidityText = "Humidity : " + humidity + "%.";
+    var windText = "Wind Speed : " + wind + " m/s.";
+    var descriptionText = "Description : " + weather.replace(/\b\w/g, function(match) {
+      return match.toUpperCase();
+    });
+
+    $("#cityName").text(cityName);
     $("#temperature").text(weatherText);
     $("#humidity").text(humidityText);
     $("#wind").text(windText);
-  });
-}
+    $("#description").text(descriptionText);
 
-// Update the time every second
-setInterval(function() {
-  var currentTime = moment().tz("{your_time_zone}").format("h:mm:ss A");
-  $("#time").text(currentTime);
-}, 1000);
+    $("#weather").css("display", "block");
+
+  })
+  .fail(function() {
+    alert("City not found.");
+  });
+
+}
 
 function clearSearch() {
   $('#city').val('');
-  document.getElementById("city").value = "";
-  document.getElementById("temperature").innerHTML = "";
-  document.getElementById("humidity").innerHTML = "";
-  document.getElementById("wind").innerHTML = "";
-}
-/*
-// Get all anchor tags on the page
-const anchors = document.getElementsByTagName('a');
+  $("#weather").css("display", "none");
+  $("#cityName").html("");
+  $("#temperature").html("");
+  $("#humidity").html("");
+  $("#wind").html("");
 
-// Loop through all anchor tags and set their title attribute to their href value
-for(let i = 0; i < anchors.length; i++) {
-  anchors[i].setAttribute('title', anchors[i].href);
-  anchors[i].href = 'javascript:void(0)';
 }
-*/
